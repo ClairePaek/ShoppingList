9109327771cc7efe402257d9e9aa3a06c6e4c984
ClairePaek <emilypaek777@gmail.com>
2022-12-11 23:01:46 +0900

    refactoring javascript

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const addBtn = document.querySelector('.footer__button');
 const input = document.querySelector('.footer__input');
 const items = document.querySelector('.items');
+let id = 0;
 
 /**
  * 아이템을 추가합니다.
@@ -31,6 +32,15 @@ input.addEventListener('keypress', (event) => {
   }
 });
 
+items.addEventListener('click', (event) => {
+  const id = event.target.dataset.id;
+
+  if (id) {
+    const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+    toBeDeleted.remove();
+  }
+});
+
 /**
  * 아이템을 생성합니다.
  * @param {string} text
@@ -39,29 +49,16 @@ input.addEventListener('keypress', (event) => {
 function createItem(text) {
   const itemRow = document.createElement('li');
   itemRow.setAttribute('class', 'item__row');
+  itemRow.setAttribute('data-id', `${id}`);
 
-  const item = document.createElement('div');
-  item.setAttribute('class', 'item');
+  itemRow.innerHTML += `<div class="item">
+                          <span class="item__name">${text}</span>
+                          <button class="item__delete">
+                            <i class='fa-solid fa-trash-can' data-id=${id}></i>
+                          </button>
+                        </div>
+                        <div class="item__divider"></div>`;
 
-  const name = document.createElement('span');
-  name.setAttribute('class', 'item__name');
-  name.innerText = text;
-
-  const deleteBtn = document.createElement('button');
-  deleteBtn.setAttribute('class', 'item__delete');
-  deleteBtn.innerHTML = "<i class='fa-solid fa-trash-can'></i>";
-  deleteBtn.addEventListener('click', (event) => {
-    items.removeChild(itemRow);
-  });
-
-  const itemDivider = document.createElement('div');
-  itemDivider.setAttribute('class', 'item__divider');
-
-  item.appendChild(name);
-  item.appendChild(deleteBtn);
-
-  itemRow.appendChild(item);
-  itemRow.appendChild(itemDivider);
-
+  id++;
   return itemRow;
 }
